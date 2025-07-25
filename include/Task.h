2ec025739733c609acee5b6c6d60fce8ee992add
STVS <eternalstud3nt@gmail.com>
2025-07-25 03:04:02 +0300

    Implement Task  class

--- include/Task.h
+++ include/Task.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+
+class Task
+{
+public:
+	Task(const std::string& title, bool done = false);
+
+	std::string getTitle() const;
+	bool isDone() const;
+
+	void markDone();
+	std::string serialize() const;
+	static Task deserialize(const std::string& line);
+
+private:
+	std::string title;
+	bool done;
+};
---
+++ src/Task.cpp
@@ -0,0 +1,44 @@
+#include "Task.h"
+#include <sstream>
+
+Task::Task(const std::string& title, bool done) : title(title), done(done) 
+{
+
+}
+
+std::string Task::getTitle() const 
+{
+	return title;
+}
+
+bool Task::isDone() const
+{
+	return done;
+}
+
+void Task::markDone() 
+{
+	done = true;
+}
+
+std::string Task::serialize() const 
+{
+	int doneInt = done ? 1 : 0;
+
+	std::ostringstream oss;
+	oss << title << "," << doneInt;
+	return oss.str();
+}
+
+Task Task::deserialize(const std::string& line) 
+{
+	std::istringstream iss(line);
+	std::string title;
+	std::string doneStr;
+	if (std::getline(iss, title, ',') && std::getline(iss, doneStr)) 
+	{
+		bool done = (doneStr == "1");
+		return Task(title, done);
+	}
+	throw std::invalid_argument("Invalid serialized task format");
+}
